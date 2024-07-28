343cd653a4c0f127117db9d0edf94001982a58f6
gianicola <gianicolajara@gmail.com>
2024-07-28 17:40:42 -0400

    auth fixed

--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import { sql } from "@vercel/postgres";
+import bcrypt from "bcrypt";
 import NextAuth, { User } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
@@ -18,10 +19,23 @@ export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
-      async authorize(credentials, request) {
+      async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+
+        console.log("Invalid credentials");
+
+        return null;
       },
     }),
   ],
